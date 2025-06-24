fdea11551a9e46503bc5e03d31af2a3d4d9a820f
Phines <nguyenquangtien2005z@gmail.com>
2025-06-24 14:53:12 +0700

    fix connect.php

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 $servername = "127.0.0.1";
 $username = "root";  // Thay bằng tên người dùng MySQL của bạn
 $password = "";        // Thay bằng mật khẩu của bạn
-$database = "cinemewebpage"; // Thay bằng tên cơ sở dữ liệu của bạn
+$database = "cinemawebpage"; // Thay bằng tên cơ sở dữ liệu của bạn
 
 // Tạo kết nối
 $conn = new mysqli($servername, $username, $password, $database);
