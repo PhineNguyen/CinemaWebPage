28c491e015b76e507d5918dec85a83f5c0bffb27
Phines <nguyenquangtien2005z@gmail.com>
2025-06-24 14:50:28 +0700

    lienhe.php

--- cinemaWebPagedatabase/DROPTABLES.sql
+++ cinemaWebPagedatabase/DROPTABLES.sql
@@ -1,14 +1,14 @@
 
 USE cinemawebpage;
 DROP TABLE food_orders;
-DROP TABLE  booking_details;
-DROP TABLE  bookings;	
+DROP TABLE booking_details;
+DROP TABLE bookings;	
 DROP TABLE seats;
-DROP TABLE  payments;
-DROP TABLE  showtimes;
+DROP TABLE payments;
+DROP TABLE showtimes;
 DROP TABLE rooms;
-DROP TABLE  cinemas;
-DROP TABLE  movies;
-DROP TABLE  users;
+DROP TABLE cinemas;
+DROP TABLE movies;
+DROP TABLE users;
 DROP TABLE food_variants;
-DROP TABLE  foods;
+DROP TABLE foods;
