ee62960011a8b49ff614c703c526eab1644e27b9
Nguyen Thanh Phuc Tuy <ntptuy1910@gmail.com>
2025-07-06 22:27:00 +0700

    thêm active admin

--- admin/sidebar_admin.php
+++ admin/sidebar_admin.php
@@ -5,16 +5,17 @@
 <!-- Sidebar -->
 <?php
   $role = isset($_SESSION['ro_lo']) ? $_SESSION['ro_lo'] : '';
+  $current = basename($_SERVER['PHP_SELF']);
 ?>
 <div class="sidebar">
   <div class="sidebar-item">
    <?php if ($role === 'admin'): ?>
-      <a href="admin.php"><i class="fa-solid fa-chart-line"></i><span>Dashboard</span></a>
-      <a href="taikhoannguoidung.php"><i class="fa-solid fa-users"></i><span>Tài khoản người dùng</span></a>
-      <a href="taikhoannhansu.php"><i class="fa-solid fa-user-tie"></i><span>Tài khoản nhân sự</span></a>
-      <a href="quanlyphim.php"><i class="fa-solid fa-film"></i><span>Quản lý phim</span></a>
-      <a href="quanlysuatchieu.php"><i class="fa-solid fa-clock"></i><span>Quản lý suất chiếu</span></a>
-      <a href="quanlyrapPC.php"><i class="fa-solid fa-building"></i><span>Quản lý rạp & Phòng chiếu</span></a>
+      <a href="admin.php" class="<?= $current=='admin.php'?'active':'' ?>"><i class="fa-solid fa-chart-line"></i><span>Dashboard</span></a>
+      <a href="taikhoannguoidung.php" class="<?= $current=='taikhoannguoidung.php'?'active':'' ?>"><i class="fa-solid fa-users"></i><span>Tài khoản người dùng</span></a>
+      <a href="taikhoannhansu.php" class="<?= $current=='taikhoannhansu.php'?'active':'' ?>"><i class="fa-solid fa-user-tie"></i><span>Tài khoản nhân sự</span></a>
+      <a href="quanlyphim.php" class="<?= $current=='quanlyphim.php'?'active':'' ?>"><i class="fa-solid fa-film"></i><span>Quản lý phim</span></a>
+      <a href="quanlysuatchieu.php" class="<?= $current=='quanlysuatchieu.php'?'active':'' ?>"><i class="fa-solid fa-clock"></i><span>Quản lý suất chiếu</span></a>
+      <a href="quanlyrapPC.php" class="<?= $current=='quanlyrapPC.php'?'active':'' ?>"><i class="fa-solid fa-building"></i><span>Quản lý rạp & Phòng chiếu</span></a>
     <?php endif; ?>
   </div>
 </div>
